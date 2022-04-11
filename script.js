f3515f77b6a094e988d203c8d173ce5b11caa190
ZJStrinic <97905725+ZJStrinic@users.noreply.github.com>
2022-04-11 17:18:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("test")
+
+
+document.querySelectorAll('.accordion_question').forEach(item => {
+    item.addEventListener('click', event => {
+
+        if(!item.classList.contains('open')) {
+            item.nextElementSibling.classList = 'accordion_collapse collapsing';
+
+            setTimeout(() => {
+                console.log('otvoreno');
+                item.nextElementSibling.classList = 'accordion_collapse open';
+            }, 300);
+        } else {
+            item.nextElementSibling.classList = 'accordion_collapse collapsing';
+
+            setTimeout(() => {
+                console.log('zatvoreno');
+                item.nextElementSibling.classList = 'accordion_collapse collapse';
+            }, 300);
+        }
+
+
+
+
+
+
+
+
+        item.classList.toggle('open');
+    })
+})
